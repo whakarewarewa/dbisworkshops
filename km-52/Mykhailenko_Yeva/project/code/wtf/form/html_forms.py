89c36e16880e31ec53fe4b17c9a038df56cf2b17
Eva <eve.mykhaylenko@gmail.com>
2018-12-25 23:29:08 +0200

    Create html_forms.py

---
+++ km-52/Mykhailenko_Yeva/project/code/wtf/form/html_forms.py
@@ -0,0 +1,70 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, validators, PasswordField, IntegerField
+
+
+class LoginForm(FlaskForm):
+    login = StringField('Login: ', validators=[validators.DataRequired('Required')])
+    password = PasswordField('Password: ', validators=[validators.DataRequired('Required')])
+
+    submit = SubmitField('Sign in')
+
+
+class RegisterSForm(FlaskForm):
+    login = StringField('Login: ', validators=[validators.DataRequired('Required'),
+                                               validators.Length(1, 20, "Login should be from 1 to 20 symbols"),
+                                               validators.Regexp('^\S+$', message='No spaces please')])
+    password = PasswordField('Password: ', validators=[validators.DataRequired('Required'),
+                                                       validators.Length
+                                                       (1, 30, "Password should be from 1 to 30 symbols")])
+    name = StringField('Name: ', validators=[validators.DataRequired('Required'),
+                                             validators.Length(1, 30, "Name should be from 1 to 30 symbols"),
+                                             validators.Regexp('^[a-zA-Z ]+$', message='Only letters please')])
+    id = StringField('Card ID: ', validators=[validators.DataRequired('Required'),
+                                              validators.Length(6, 6, "6 symbols allowed"),
+                                              validators.Regexp('^[0-9]', message='Only numeric')])
+    st_semester = IntegerField('Study Semester: ', validators=[validators.DataRequired('Require a number'),
+                                                               validators.NumberRange(min=1, max=10,
+                                                                                      message='From 1 to 10')])
+    submit = SubmitField('Sign up')
+
+
+class RegisterTForm(FlaskForm):
+    login = StringField('Login: ', validators=[validators.DataRequired('Required'),
+                                               validators.Length(1, 20, "Login should be from 1 to 20 symbols"),
+                                               validators.Regexp('^\S+$', message='No spaces please')])
+    password = PasswordField('Password: ', validators=[validators.DataRequired('Required'),
+                                                       validators.Length
+                                                       (1, 30, "Password should be from 1 to 30 symbols")])
+    name = StringField('Name: ', validators=[validators.DataRequired('Required'),
+                                             validators.Length(1, 30, "Name should be from 1 to 30 symbols"),
+                                             validators.Regexp('^[a-zA-Z ]+$', message='Only letters please')])
+    id = StringField('Contract ID: ', validators=[validators.DataRequired('Required'),
+                                                  validators.Length(6, 6, "6 symbols allowed"),
+                                                  validators.Regexp('^[0-9]', message='Only numeric')])
+
+    submit = SubmitField('Sign up')
+
+
+class FindDisciplineForm(FlaskForm):
+    discipline = StringField('Enter discipline title in CAPS:',
+                             validators=[validators.DataRequired(), validators.Length(1, 30),
+                                         validators.Regexp('^[A-Z ]+$', message='Only big letters please')])
+    submit = SubmitField('Find')
+
+
+class EditPasswordForm(FlaskForm):
+    password = PasswordField('', validators=[validators.DataRequired('Required'),
+                                             validators.Length(1, 30, "Should be between 1 and 30")])
+    submit = SubmitField('Update')
+
+
+class DeleteUserForm(FlaskForm):
+    delete = SubmitField('Delete profile')
+
+
+class AddDisciplineForm(FlaskForm):
+    dscpln_name = StringField('Discipline: ', validators=[validators.DataRequired('Required'),
+                                                          validators.Length(1, 30, "Should be between 1 and 30"),
+                                                          validators.Regexp('^[A-Z ]+$', message='Only big letters please')])
+
+    submit = SubmitField('Add')
